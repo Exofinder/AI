5500097a46bb9e880ef4f0b33069a6dc1ae191bf
choyoungseo20 <whdudtj0809@naver.com>
2024-10-05 12:21:36 +0900

    Set Project Frame

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import List
+from havitable_carculator import calculate_habitable_percent as chp
+
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/systemInfo")
+async def carculateHabitable(
+        planetNames: List[str] = Query(None),
+        hostNmaes: List[str] = Query(None),
+        distances: List[str] = Query(None),
+        spectralTypes: List[str] = Query(None),
+        VMagnitudes: List[str] = Query(None),
+        planetDensities: List[str] = Query(None),
+        hostdensities: List[str] = Query(None),
+        eccentricities: List[str] = Query(None)):
 
+    results = []
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    for i in range(len(planetNames)):
+        result = chp(
+            planetNames[i],
+            hostNmaes[i],
+            distances[i],
+            spectralTypes[i],
+            VMagnitudes[i],
+            planetDensities[i],
+            hostdensities[i],
+            eccentricities[i]
+        )
+        results.append(result)
+
+    return results
