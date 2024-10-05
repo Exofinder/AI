26fbc4b685761bfc91f08a4ef86bfb24210dd775
Seo Boseung <sbs7696@naver.com>
2024-10-05 17:13:49 +0900

    str -> float

--- habitable_carculator.py
+++ habitable_carculator.py
@@ -1,14 +1,23 @@
+import fomula
+
 def calculate_habitable_percent(
         planet: str,
-        host: str,
         distance: str,
         spectralType: str,
         VMagnitude: str,
         planetDensity: str,
         hostDensity: str,
-        eccentricity: str):
+        eccentricity: str
+        
+        
+        ):
     ########## AI logic ##########
-
+    distance=float(distance)
+    VMagnitude=float(VMagnitude)
+    planetDensity=float(planetDensity)
+    hostDensity=float(hostDensity)
+    eccentricity=float(eccentricity)
+    
     habitablePercent = ()
 
     ########## AI logic ##########
