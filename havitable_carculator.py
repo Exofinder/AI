3dbef050789bbd8c87ded562ff2c4bcd061ea15a
choyoungseo20 <whdudtj0809@naver.com>
2024-10-05 12:21:42 +0900

    Set Project Frame

---
+++ havitable_carculator.py
@@ -0,0 +1,19 @@
+def calculate_habitable_percent(
+        planet: str,
+        host: str,
+        distance: str,
+        spectralType: str,
+        VMagnitude: str,
+        planetDensity: str,
+        hostDensity: str,
+        eccentricity: str):
+    ########## AI logic ##########
+
+    habitablePercent = ()
+
+    ########## AI logic ##########
+
+    return {
+        "planet": planet,
+        "habitablePercent": habitablePercent
+    }
